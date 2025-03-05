ee4abc7dae2f453953f6d813955ff65e039e0783
Juan Fco Recio <jfrecio@gmail.com>
2025-03-05 11:59:41 +0100

    Primera subida

---
+++ src/main/java/org/example/helloWorld.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import org.apache.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class helloWorld
+{
+    public static void main( String[] args )
+
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(StringUtils.toUpperCase("hello world"));
+    }
+}
